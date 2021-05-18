123965e3992155e0c830dfefd9f5b56340d934e1
rhmt-ahmadi <rhmt.ahmadi@gmail.com>
2021-05-18 17:01:19 +0430

    Ifix template menu

--- resources/js/components/hero-section/HeroSidebar.vue
+++ resources/js/components/hero-section/HeroSidebar.vue
@@ -3,8 +3,8 @@
 
             <div class="md:mt-12 md:w-48 md:fixed md:left-0 md:top-0 content-center md:content-start text-left justify-between">
                 <ul class="list-reset flex flex-row md:flex-col py-0 md:py-3 px-1 md:px-2 text-center md:text-left">
-                    <li class="mr-3 flex-1">
-                        <router-link to="/" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
+                    <li class="mr-3 flex-1 ">
+                        <router-link to="/" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline border-b-2 border-gray-800 hover:border-pink-500  ">
                          <i class="fas fa-home pr-0 md:pr-0 text-blue-600"></i><span class="pb-1 md:pb-0 text-xs      md:text-base text-black md:text-black block md:inline-block pl-5">Home</span>
                          </router-link>
                     </li>
